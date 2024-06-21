be49e9049bcc8543ddf76e0625c3e4a0dfca2f08
gy___ <nqaisme@icloud.com>
2024-06-21 15:55:14 +0700

    done women's ready to wear

--- script/main.js
+++ script/main.js
@@ -41,3 +41,203 @@ search_icon.addEventListener('click', () => {
 
 
 
+
+//handle slide show
+
+
+
+const slideShows = document.querySelectorAll('.product-popup--slide')
+slideShows.forEach(slide => {
+
+    /*This section for responsive*/
+    //get slide-list of slide
+    const imageList = slide.querySelector('.slide-list')
+
+
+
+    /*This section is for the mobile*/
+    const mobileDevice = window.matchMedia('(min-width: 320px) and (max-width: 767px)')
+    const tabletAndDesktopDevice = window.matchMedia('(min-width: 768px)')
+
+    //
+    let currentPlace = slide.offsetWidth * -1, previousPlace = currentPlace, startX = 0, currentX = 0, deltaX = 0, currentIndex = 1
+
+
+    //handle touch event
+
+
+
+
+    function handleTouchStart(touchEvent) {
+        if (mobileDevice.matches) {
+            startX = touchEvent.touches[0].clientX
+            slide.addEventListener('touchmove', handleTouchMove)
+            slide.addEventListener('touchend', handleTouchEnd)
+        }
+    }
+
+    function handleTouchMove(touchEvent) {
+        currentX = touchEvent.touches[0].clientX
+        deltaX = currentX - startX
+        currentPlace = previousPlace + deltaX
+        setSlideByPlace()
+    }
+
+    function handleTouchEnd(touchEvent) {
+        slide.removeEventListener('touchmove', handleTouchMove)
+        let movedDistance = currentPlace - previousPlace
+        if (movedDistance * -1 > imageList.offsetWidth / 20 && currentIndex < imageList.children.length - 2) currentIndex++
+        if (movedDistance > imageList.offsetWidth / 20 && currentIndex > 1) currentIndex--
+        setSlideByIndex()
+        handleSlideProcess()
+    }
+
+
+    //setup slide
+    function setSlideByPlace() {
+        imageList.style.transform = `translateX(${currentPlace}px)`
+    }
+
+
+    function handleSlideProcess() {
+        const process = slide.querySelectorAll('.process__dot')
+        process.forEach(dot => {
+            dot.classList.remove('process__active')
+        })
+        process[currentIndex - 1].classList.add('process__active')
+    }
+
+    function setSlideByIndex() {
+        currentPlace = currentIndex * imageList.offsetWidth * -1
+        previousPlace = currentPlace
+        imageList.style.transform = `translateX(${currentIndex * imageList.offsetWidth * -1}px)`
+    }
+
+
+    //handle change device
+    function handleChangeDevie() {
+        resetSize()
+        if (mobileDevice.matches) {
+            currentPlace = imageList.offsetWidth * -1
+            previousPlace = currentPlace
+            deltaX = 0
+            startX = 0
+            currentX = 0
+        }
+        resetToIndex(1)
+    }
+
+
+    function resetToIndex(index) {
+        imageList.style.transition = 'none'
+        currentIndex = index
+        setSlideByIndex()
+        handleSlideProcess()
+        setTimeout(() => {
+            imageList.style.transition = '.4s ease-in-out'
+        }, 400)
+    }
+
+    function resetSize() {
+        imageList.style.width = `${slide.offsetWidth}px`
+    }
+
+    window.addEventListener('resize', handleChangeDevie)
+    window.addEventListener('load', resetSize)
+
+    slide.addEventListener('touchstart', handleTouchStart)
+
+
+
+
+
+    /*This section for the tablet and desktop devices*/
+    //get control buttons
+    const next = slide.querySelector('.next')
+    const prev = slide.querySelector('.prev')
+
+
+    //handle mouse event
+    function handleMouseOver(mouseEvent) {
+        if (tabletAndDesktopDevice.matches) {
+            currentIndex = 2
+            setSlideByIndex()
+            displayControlButton()
+            slide.addEventListener('mouseleave', handleMouseLeave)
+        }
+    }
+
+    function handleMouseLeave(mouseEvent) {
+        resetToIndex(1)
+        hideControlButton()
+    }
+
+
+    //handle control buttons display
+    function displayControlButton() {
+        slide.querySelectorAll('.slide-list--control').forEach(button => {
+            button.style.visibility = 'visible'
+        })
+    }
+    function hideControlButton() {
+        slide.querySelectorAll('.slide-list--control').forEach(button => {
+            button.style.visibility = 'hidden'
+        })
+    }
+
+
+
+    //handle control buttons
+    let isClickable = true
+    function nextSlide() {
+        if (isClickable) {
+            isClickable = false
+            if (currentIndex < imageList.children.length - 1) {
+                currentIndex++
+                setSlideByIndex()
+            }
+            if (currentIndex == imageList.children.length - 1) {
+                setTimeout(() => {
+                    imageList.style.transition = 'none'
+                    currentIndex = 1
+                    setSlideByIndex()
+                }, 400)
+            }
+            imageList.style.transition = '.4s ease-in-out'
+
+            setTimeout(() => {
+                isClickable = true
+            }, 400)
+        }
+    }
+
+    function prevSlide() {
+        if (isClickable) {
+            isClickable = false
+            if (currentIndex > 0) {
+                currentIndex--
+                setSlideByIndex()
+            }
+            if (currentIndex == 0) {
+                setTimeout(() => {
+                    imageList.style.transition = 'none'
+                    currentIndex = imageList.children.length - 2
+                    setSlideByIndex()
+                }, 400)
+            }
+            imageList.style.transition = '.4s ease-in-out'
+            setTimeout(() => {
+                isClickable = true
+            }, 400)
+        }
+    }
+
+
+    function setSlideByIndex_Desktop() {
+        imageList.style.transform = `translateX(${currentIndex * imageList.offsetWidth * -1}px)`
+    }
+    next.addEventListener('click', nextSlide)
+    prev.addEventListener('click', prevSlide)
+    slide.addEventListener('mouseenter', handleMouseOver)
+
+})
